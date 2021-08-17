b01852b23936b48614426eb1c2d1c9621d89eb45
pratyushlokhande <pratyush3141@gmail.com>
2021-08-18 00:09:02 +0530

    basic structuring done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import React, { useState } from "react";
 
+// Importing Styles
+import "./styles/app.scss";
+
+// Importing Components
+import Song from "./components/Song";
+import Player from "./components/Player";
+
+// Import util
+import data from "./util";
 
 function App() {
+  // State
+  const [songs, setSong] = useState(data());
+  const [currentSong, setCurrentSong] = useState(data()[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <div className="App">
-      <h1>Music Player</h1>
+      <Song currentSong={currentSong} />
+      <Player
+        currentSong={currentSong}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+      />
     </div>
   );
 }
